bd059107527da25bf457dd0592cd769cbe313f42
kvngnandom <davidjseph3@gmail.com>
2023-02-22 15:05:48 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the times table
+ * @n: integer for which the times table will be printed
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_times_table(int n)
+{
